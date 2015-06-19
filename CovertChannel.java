f91477ec613ddf17b8672d66cd749649e5fd291e
Gavin <gavindl@cs.utexas.edu>
2015-06-19 11:09:22 -0500

    added verbose T/F statement to make log inputing easier. Added the out file for input file, still nothing read in there

--- CovertChannel.java
+++ CovertChannel.java
@@ -7,16 +7,24 @@ public class CovertChannel{
 	public static final int NUM_INSTRUCTIONS = 4;
 	public static final int NUM_SUBJECTS = 2;
 	public static OutputStream output;
+	public static OutputStream out; //file for output
+	static boolean v = false;
 	public static void main(String args[]) throws Exception{
 
 	    String[] instructions = {"create hal obj, create lyle obj, write lyle obj 1, read lyle obj, destroy lyle obj, run lyle"};
 	    Bytes bytes;
 	    if(args[0].equals("v")){
+		System.out.println("Reading from file: " +args[1]);
+		v = true;
+		out = new FileOutputStream(args[1] +".out");
 		output = new FileOutputStream(new File("log"));
 		bytes = new Bytes(args[1]);
 		
 	    }
 	    else{
+		v = false;
+		out = new FileOutputStream(args[0]+".out");
+		System.out.println("Reading from file: "+args[0]);
 	    	bytes = new Bytes(args[0]);
 	    }
 	    String[] bits_array;
@@ -32,6 +40,9 @@ public class CovertChannel{
 	    		ref.setInstruction(instructionObject);
 	    		
 	    	}
+
+
+/* Need some sort of timer for bits/ms. */
 
 
 	    }
